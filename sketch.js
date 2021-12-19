91f096658acb385cbbdfcad04c09b64e22edec8d
sohamsengupta07 <93522481+sohamsengupta07@users.noreply.github.com>
2021-12-19 20:00:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -67,8 +67,8 @@ function setup(){
     // ball = Bodies.circle(590,100,20);
     // World.add(myworld,ball);
 
-    // ball = Bodies.ellipse(590,100,20,
-    //                      ball_options);
+     ball = Bodies.ellipse(590,100,20,
+                  ball_options);
     // World.add(myworld,ball);
 
     // ball = Bodies.circle(590,100,20,
